a8f4632c9082ce5bbd2aaa07d31539b1cdcb708a
farouk karti <faroukelrey19@gmail.com>
2025-11-24 13:17:22 +0100

    Add metrics.php

--- backend/app/Services/PrometheusService.php
+++ backend/app/Services/PrometheusService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Prometheus\CollectorRegistry;
 use Prometheus\Storage\InMemory;
+use Prometheus\RenderTextFormat;
 
 class PrometheusService
 {
@@ -11,11 +12,19 @@ class PrometheusService
 
     public function __construct()
     {
+        // Use shared in-memory store
         $this->registry = new CollectorRegistry(new InMemory());
     }
 
     public function getRegistry()
     {
         return $this->registry;
+    }
+
+    // REQUIRED: return all metrics in Prometheus format
+    public function getMetrics()
+    {
+        $renderer = new RenderTextFormat();
+        return $renderer->render($this->registry->getMetricFamilySamples());
     }
 }
